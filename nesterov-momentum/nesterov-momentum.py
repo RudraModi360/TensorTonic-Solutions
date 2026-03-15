1c7e998b5682f68dd5a6bece85b97a3190347a00
Rudra_Modi@360 <130299388+RudraModi360@users.noreply.github.com>
2026-03-15 12:11:32 +0530

    Add solution for nesterov-momentum

---
+++ nesterov-momentum/nesterov-momentum.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def nesterov_momentum_step(w, v, grad, lr=0.01, momentum=0.9):
+    """
+    Perform one Nesterov Momentum update step.
+    """
+    w=np.array(w)
+    v=np.array(v)
+    grad=np.array(grad)
+
+    w_look=w-momentum*v
+
+    v_new=momentum*v+(lr*grad)
+
+    w_new=w-v_new
+
+    return w_new,v_new
